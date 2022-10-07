7c4adb8080ccc0828fb0d099cd2ee9aaa3e7a118
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-07 18:37:17 +0100

    started working on ft_bzero.c

---
+++ ft_bzero.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 18:35:11 by yel-hadd          #+#    #+#             */
+/*   Updated: 2022/10/07 18:36:51 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <strings.h>
+
+void	bzero(void *s, size_t n)
+{
+	//amazing code goes here
+}
+
+int main(void)
+{
+	//amazing code goes here
+}
