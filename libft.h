5e8057daef3efd762a8e3d199f87a7f17c34851d
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-08 16:33:57 +0100

    update header file with recently added functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 22:47:39 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/07 20:47:41 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/10/08 16:33:34 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ char    *ft_strrchr(char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 #endif
