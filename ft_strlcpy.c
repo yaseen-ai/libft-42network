11d663cc467af1d3cb5ad1d9556a9c394f2aa11e
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-10 22:22:07 +0100

    Create ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 22:20:55 by yel-hadd          #+#    #+#             */
+/*   Updated: 2022/10/10 22:22:00 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+	// Amazing code goes here
+}
