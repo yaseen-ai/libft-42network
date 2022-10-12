353b42fec243a4efc5a88eb8a7961e8cf283bd90
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-12 21:42:18 +0100

    progress

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,8 +6,21 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 21:05:23 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/12 21:05:25 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/10/12 21:42:09 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dstlen;
+	size_t	srclen;
+	size_t	counter;
 
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	dst += dstlen;
+	counter = dstsize - dstlen - 1
+
+	while (*src && counter --)
+		* src ++ = *dst ++;
+}
