15ca1d97c52096757c8e08c166002c2b0996701f
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-13 00:33:43 +0100

    progress

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,29 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 22:57:00 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/12 22:58:09 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/10/13 00:33:34 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *strnstr(const char *haystack, const char *needle, size_t len)
+#include "libft.h"
+
+char *strnstr(const char *s1, const char *s2, size_t len)
 {
-	//amazing code goes here
+	size_t	len;
+	size_t	i;
+
+	len = s2;
+	if (len == 0)
+		return (s1);
+	while(*s1 ++)
+	{
+		i = 0;
+		while (*s1+i == *s2+i)
+		{
+			if (i == len)
+				return (s1 - i);
+			i ++;
+		}
+	}
+	return (0);
 }
