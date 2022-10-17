c8d80b4030d70c0802d63af21b53f38d8746b512
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-17 17:31:14 +0100

    create an initial version of ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,8 +6,35 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:58:57 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/17 16:58:59 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/10/17 17:30:43 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "libft.h"
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*sub;
+
+	i = 0;
+	sub = malloc((len - start) * sizeof(char));
+	if (!sub)
+		return (0);
+	while ((start + i) <= len)
+	{
+		sub[i] = s[start + i];
+		i ++;
+	}
+	return (sub);
+}
+
+/*
+int	main(void)
+{
+	char const	*str1 = {"Hello World"};
+
+	printf("%s", ft_substr(str1, (unsigned int) 6, ft_strlen(str1)));
+}
+*/
