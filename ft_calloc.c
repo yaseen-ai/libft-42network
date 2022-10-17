eec80dd40405318eab4e89a016c97abfa2ee5621
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-17 16:32:07 +0100

    initial version of ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,8 +6,21 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 15:12:46 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/17 15:12:48 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/10/17 16:30:47 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	void	*ptrcpy;
+
+	ptr = malloc(size * count);
+	ptrcpy = ptr;
+	if (!ptr)
+		return (0);
+	ft_bzero(ptr, count);
+	return (ptrcpy);
+}
