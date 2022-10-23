d3ccc35af6112c17a755b3b2f7549a6f6bb3c91e
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-23 17:10:52 +0100

    fixed issues

--- libft.h
+++ libft.h
@@ -29,7 +29,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dst, const void  *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_isspace(char c);
 int		ft_atoi(const char *str);
