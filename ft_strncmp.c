a86194dc119a274a44a33570f0f714b716b99b31
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-24 18:52:54 +0100

    fixed bugs

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,15 +14,19 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n != 0)
+	if (!ft_strlen(s1))
+		return ((unsigned char) *s1 - (unsigned char)*s2);
+	if (!ft_strlen(s2))
+		return ((unsigned char) *s1 - (unsigned char)*s2);
+	while (*s1 && (n != 0))
 	{
 		if (*s1 != *s2)
-			return (*s1 - *s2);
+			return ((unsigned char) *s1 - (unsigned char)*s2);
 		n --;
 		s1 ++;
 		s2 ++;
 	}
-	return (0);
+	return ((unsigned char) 0);
 }
 
 /*
@@ -31,13 +35,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 int main(void)
 {
-	char	*str1 = {"Hello World"};
-	char	*str2 = {"HelloaWorld"};
+	char	*str1 = {"jfejfhbeifn"};
+	char	*str2 = {""};
 	int		num = 8;
 
+	printf("%d", strncmp(str1, str2, num));
 	if (strncmp(str1, str2, num) == ft_strncmp(str1, str2, num))
 		printf("IDENTICAL BRO");
 	else
 		printf(" NOT IDENTICAL, KEEP TRYING!");
+	
 }
 */
