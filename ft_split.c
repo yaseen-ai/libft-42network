9dd0e01cf3ecc528cc30a8c5c5aa952ac4dc54d3
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-27 19:35:03 +0100

    some progress, fix bugs later

--- ft_split.c
+++ ft_split.c
@@ -13,3 +13,74 @@
 #include "libft.h"
 #include <stdio.h>
 
+static char	*getnextstart(char *str, char c)
+{
+	while (*str != c && *str)
+		str ++;
+	while (*str == c && *str)
+		str ++;
+	return (str);
+}
+
+static size_t	getnextstop(char *str, char c)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(str);
+	while (str[i] != c && i < len)
+		i ++;
+	return (i);
+}
+
+static char	**fill(char **result, char *copy, char c, size_t count)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < count)
+	{
+		result[i] = ft_substr(copy, 0, getnextstop(copy, c));
+		copy = getnextstart(copy, c);
+		i ++;
+	}
+	result[i] = (char *) NULL;
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	*copy;
+	char	**result;
+	size_t	i;
+	size_t	count;
+
+	if (!s || !c)
+		return (NULL);
+	i = 0;
+	count = 1;
+	copy = (char *)s;
+	copy = ft_strtrim(copy, &c);
+	while (copy[i])
+	{
+		if (copy[i] == c && copy[i + 1] != c)
+			count ++;
+		i ++;
+	}
+	free(copy);
+	result = malloc((count + 1) * sizeof(char *));
+	result = fill(result, copy, c, count);
+	return (result);
+}
+
+/*
+int main(void)
+{
+    char *s = {",,,,,,,,ello,,,,,,World,1,3444444444,*,,,,,,"};
+    char c = ',';
+    char **result = NULL;
+    result = ft_split(s, c);
+    printf("%s\n", result[0]);
+}
+*/
