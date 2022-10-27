8ffd62eec049abbdff1cf7ba31d4fe9822bdb2b5
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-27 13:33:16 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -36,5 +36,6 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strdup(const char *s1);
 
 #endif
