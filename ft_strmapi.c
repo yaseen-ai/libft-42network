825ff4ca4f8a6a1c420a694dc47a83924d20c697
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-29 19:31:39 +0100

    👌

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -11,4 +11,25 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*result;
+	unsigned int	i;
+	size_t			len;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	result = (char *) malloc((len + 1) * sizeof(char));
+	if (!result || !s)
+		return (result);
+	i = 0;
+	while (*s)
+	{
+		result[i] = f(i, *s++);
+		i ++;
+	}
+	result[i] = '\0';
+	return (result);
+}
