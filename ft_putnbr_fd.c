9bfd465bdc0ee4fd59a7a6134aba3751fafbca97
Yassine El Haddad <megahaddad15@gmail.com>
2022-10-31 13:54:48 +0100

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,31 +6,36 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 13:13:21 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/30 13:29:12 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/10/31 12:45:15 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
+	char	zero;
+
+	zero = '0';
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", (11 * sizeof(char)));
+		n = -n;
+		return ;
+	}
 	if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
+		write(fd, "-", 1);
+		n = -n;
 	}
-	if (n == 0)
-		ft_putchar_fd(48, fd);
 	if (n <= 9)
-		ft_putchar_fd((48 + n), fd);
+	{
+		zero += n;
+		write(fd, &zero, sizeof(char));
+	}
 	else
 	{
-		ft_putchar_fd((48 + (n / 10)), fd);
-		ft_putchar_fd((48 + (n % 10)), fd);
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
-}
-
-int	main(void)
-{
-	ft_putnbr_fd(115, 1);
 }
