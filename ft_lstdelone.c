414d6a36d13cec4fe395dfecb4d096468f145260
Yassine El Haddad <megahaddad15@gmail.com>
2022-11-04 13:12:43 +0100

    👌

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,34 +6,28 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 01:52:45 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/11/03 19:46:54 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/11/04 12:54:59 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstdelone(t_list *lst, void (*del)(void*))
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (!lst || !del)
-        return ;
-    del(lst->content);
+		return ;
+	del(lst->content);
+	free(lst);
 }
-
 /*
-static void ft_free(void *node)
-{
-   free(node);
-   printf("*\n");
-}
-
 int main(void)
 {
 	t_list	*list;
 	t_list	*new;
-	char	*str = {"AB"};
-    void (*delete)(void*) = &ft_free;
+    void (*delete)(void*) = &free;
 
-	list = ft_lstnew(str);
+	list = malloc(sizeof(t_list *));
+	list = ft_lstnew(ft_strdup("AB"));
 	new = ft_lstnew(ft_strdup("CD"));
 	ft_lstadd_back(&list, new);
     ft_lstdelone(list->next, delete);
