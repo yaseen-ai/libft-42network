720a06435df3f879d90d6577442034d9ca669063
Yassine El Haddad <megahaddad15@gmail.com>
2022-11-04 13:13:20 +0100

    Update ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,11 +6,12 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 19:50:46 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/11/03 20:27:24 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/11/04 13:07:01 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
@@ -23,7 +24,24 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	{
 		*lst = (*lst)->next;
 		ft_lstdelone(tmp, del);
-		free(tmp);
 		tmp = *lst;
 	}
 }
+/*
+int main(void)
+{
+	t_list	*list;
+	t_list	*new;
+    void (*delete)(void*) = &free;
+
+	list = malloc(sizeof(t_list *));
+	list = ft_lstnew(ft_strdup("AB"));
+	new = ft_lstnew(ft_strdup("CD"));
+	ft_lstadd_back(&list, new);
+    ft_lstclear(&list, delete);
+	while (list)
+	{
+		printf("%s\n", list->content);
+		list = list->next;
+	}
+}*/
