49d7fd0ed50bbb72dee4d4ce075b98522ab9018c
Yassine El Haddad <megahaddad15@gmail.com>
2022-11-05 14:01:21 +0100

    Update ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,8 +6,17 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 13:11:44 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/11/04 13:11:45 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/11/04 13:32:41 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
