27d237a59309331beb23b4c644ff1e11222eea59
Yassine El Haddad <megahaddad15@gmail.com>
2022-11-07 00:55:45 +0100

    👌

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 12:23:41 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/07 13:18:11 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/11/06 13:20:16 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (!ft_strlen(s1))
-		return ((unsigned char) *s1 - (unsigned char)*s2);
-	if (!ft_strlen(s2))
-		return ((unsigned char) *s1 - (unsigned char)*s2);
-	while (*s1 && (n != 0))
-	{
-		if (*s1 != *s2)
-			return ((unsigned char) *s1 - (unsigned char)*s2);
-		n --;
-		s1 ++;
-		s2 ++;
-	}
-	return ((unsigned char) 0);
-}
+	size_t			i;
+	unsigned char	*uc1;
+	unsigned char	*uc2;
 
+	uc1 = (unsigned char *) s1;
+	uc2 = (unsigned char *) s2;
+	i = 0;
+	while ((uc1[i] || uc2[i]) && i < n)
+	{
+		if (uc1[i] != uc2[i])
+			return (uc1[i] - uc2[i]);
+		i ++;
+	}
+	return (0);
+}
 /*
 #include <stdio.h>
 #include <string.h>
