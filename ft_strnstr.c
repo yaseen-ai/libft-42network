855a614fc7430b9bf7d624b79634c09d36c8fbc5
Yassine El Haddad <megahaddad15@gmail.com>
2022-11-07 02:12:36 +0100

    👌

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,38 +6,40 @@
 /*   By: yel-hadd <yel-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 15:01:23 by yel-hadd          #+#    #+#             */
-/*   Updated: 2022/10/16 16:31:23 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2022/11/07 02:12:16 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	hlen;
 	size_t	nlen;
 	size_t	i;
-	size_t	j;
+	size_t	y;
+	char	*copy;
 
+	copy = (char *) haystack;
+	hlen = ft_strlen(haystack);
 	nlen = ft_strlen(needle);
-	if (!nlen || (haystack == needle) || !len)
-		return ((char *) haystack);
-	j = 0;
-	while (*haystack ++ && j < len)
+	if (!nlen)
+		return (copy);
+	if (hlen < nlen)
+		return (NULL);
+	i = 0;
+	while (copy[i] && i < len)
 	{
-		i = 0;
-		while (haystack[i] == needle[i])
+		y = 0;
+		while ((needle[y] == copy[i + y]) && (y + i < len))
 		{
-			i += 1;
-			j ++;
+			if (!needle[++ y])
+				return (copy + i);
 		}
-		if (i == nlen && j < len)
-			return ((char *) haystack);
+		i ++;
 	}
 	return (NULL);
 }
-
 /*
 #include <string.h>
 #include <stdio.h>
