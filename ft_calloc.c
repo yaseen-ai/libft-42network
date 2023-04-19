3152da980c391de93eabffd0ae18ea733e33009b
Yassine El Haddad <yel-hadd@mail.com>
2023-04-19 23:49:34 +0000

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	ptr = ft_malloc(size * count);
+	ptr = malloc(size * count);
 	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, count * size);
